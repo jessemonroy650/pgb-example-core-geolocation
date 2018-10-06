95d90288522c49f6a2c247f4ef6726cbf7aa4075
jessemonroy650 <jessemonroy650@yahoo.com>
2018-10-06 17:12:12 -0600

    update output

--- geolocation.js
+++ geolocation.js
@@ -19,7 +19,16 @@ var geolocationPlugin = {
         document.getElementById('status0').innerHTML = "getCurrentPosition() called";
         //
         navigator.geolocation.getCurrentPosition(
-            function () {
+            function (position) {
+                document.getElementById('geolocationStuff').innerHTML =
+                    'Latitude: '          + position.coords.latitude          + '<br />' +
+                    'Longitude: '         + position.coords.longitude         + '<br />' +
+                    'Altitude: '          + position.coords.altitude          + '<br />' +
+                    'Accuracy: '          + position.coords.accuracy          + '<br />' +
+                    'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br />' +
+                    'Heading: '           + position.coords.heading           + '<br />' +
+                    'Speed: '             + position.coords.speed             + '<br />' +
+                    'Timestamp: '         + position.timestamp;
             }, 
             geolocationPlugin.onError,
             { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }
@@ -31,7 +40,16 @@ var geolocationPlugin = {
         document.getElementById('status0').innerHTML = "watchPosition() called";
         //
         geolocationPlugin.watchID = navigator.geolocation.watchPosition(
-            function () {
+            function (position) {
+                document.getElementById('geolocationStuff').innerHTML =
+                    'Latitude: '          + position.coords.latitude          + '<br />' +
+                    'Longitude: '         + position.coords.longitude         + '<br />' +
+                    'Altitude: '          + position.coords.altitude          + '<br />' +
+                    'Accuracy: '          + position.coords.accuracy          + '<br />' +
+                    'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br />' +
+                    'Heading: '           + position.coords.heading           + '<br />' +
+                    'Speed: '             + position.coords.speed             + '<br />' +
+                    'Timestamp: '         + position.timestamp;
             }, 
             geolocationPlugin.onError,
             { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }
